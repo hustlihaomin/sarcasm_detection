be70ba18f85b9388da411c1afe613d4f9e2ee181
lihaomin <lihaomin123@qq.com>
2021-05-06 11:15:33 +0800

    fix(/train):fix lstm_attention model can't save
    -must give the right model's name

--- train/lstmattention.py
+++ train/lstmattention.py
@@ -4,7 +4,7 @@ from train.base import Train
 class LSTMAttentionTrain(Train):
     def __init__(self, input_dimensions, input_length, output_classes, net, log_path, model_path, devices,
                  dataset, early_stop):
-        super().__init__(input_dimensions, input_length, output_classes, net, log_path, model_path, devices, 'LSTM',
+        super().__init__(input_dimensions, input_length, output_classes, net, log_path, model_path, devices, 'LSTMAttention',
                          dataset, early_stop)
 
     def do_train(self, dataloader, parameters, scheduler=None, learning_rate=1e-5, weight_decay=1e-2, patience=10):
